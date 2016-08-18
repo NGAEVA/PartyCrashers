c40322f3bbfbcf30fa857c5263f8df443930e979
Mxeaez <brodyball@shaw.ca>
2016-08-18 07:21:11 -0700

    Camera working for 2 players

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,7 +6,6 @@ public class CameraController : MonoBehaviour {
     public Vector3 rotation = new Vector3(45, 0, 0);
     public int height = 10;
     public int distanceOffset = 10;
-    private bool stopCamera = false;
 
     GameObject[] players;
 	// Use this for initialization
@@ -27,31 +26,9 @@ public class CameraController : MonoBehaviour {
         {
             x += players[i].gameObject.transform.position.x;
             z += players[i].gameObject.transform.position.z;
-            if(players[i].GetComponent<PlayerController>().stopCamera == true)
-            {
-                stopCamera = true;
-            }
         }
 
-        if (stopCamera == false)
-        {
-             gameObject.transform.position = new Vector3(x / players.Length, transform.position.y, z / players.Length - distanceOffset);
-        }
-        stopCamera = false;
-    }
+        gameObject.transform.position = new Vector3(x / players.Length, transform.position.y, z / players.Length - distanceOffset);
 
-    bool cameraIsNotCentered(float x, float z, int numPlayers)
-    {
-        float averageX = x / numPlayers;
-        float averageZ = z / numPlayers - distanceOffset;
-
-        if(Camera.main.transform.position.x != averageX || Camera.main.transform.position.z != averageZ)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class PlayerController : MonoBehaviour
 {
-    public bool stopCamera = false;
 
     public float speed = 5.0f;
     public float turnSpeed = 5.0f;
     public float gravity = 100f;
     public float jump = 30.0f;
 
+    public float maxMovementX = 14f;
+    public float maxMovementZ = 18f;
+
     public string jumpButton = "Jump_P1";
     public string horizontalButton = "Horizontal_P1";
     public string verticalButton = "Vertical_P1";
 
     private Vector3 moveDir = Vector3.zero;
+
+    private GameObject[] players;
+
+    private bool stopMovementX = false;
+    private bool stopMovementZ = false;
 
     //private Rigidbody rigidBody;
     CharacterController controller;
@@ -24,14 +32,34 @@ public class PlayerController : MonoBehaviour
     {
         //rigidBody = gameObject.GetComponent<Rigidbody>();
         controller = gameObject.GetComponent<CharacterController>();
+        players = GameObject.FindGameObjectsWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        checkMovement();
         if (controller.isGrounded)
         {
-            moveDir = new Vector3(Input.GetAxis(horizontalButton), 0, Input.GetAxis(verticalButton));
+            if (stopMovementX == false && stopMovementZ == false)
+            {
+                moveDir = new Vector3(Input.GetAxis(horizontalButton), 0, Input.GetAxis(verticalButton));
+            }
+            //cant move in x direction
+            else if(stopMovementX == true && stopMovementZ == false)
+            {
+                moveDir = new Vector3(0, 0, Input.GetAxis(verticalButton));
+            }
+            //cant move in z direction
+            else if(stopMovementX == false && stopMovementZ == true)
+            {
+                moveDir = new Vector3(Input.GetAxis(horizontalButton), 0, 0);
+            }
+            //cant move in either direction
+            else if(stopMovementX == true && stopMovementZ == true)
+            {
+                moveDir = new Vector3(0, 0, 0);
+            }
             //moveDir = transform.TransformDirection(moveDir);
             moveDir *= speed;
             if (Input.GetButton(jumpButton))
@@ -48,19 +76,70 @@ public class PlayerController : MonoBehaviour
         pos.x = Mathf.Clamp01(pos.x);
         pos.y = Mathf.Clamp01(pos.y);
         transform.position = Camera.main.ViewportToWorldPoint(pos);
-
-        if(Camera.main.WorldToViewportPoint(transform.position).x < .05 || Camera.main.WorldToViewportPoint(transform.position).x > .95
-            || Camera.main.WorldToViewportPoint(transform.position).y < .05 || Camera.main.WorldToViewportPoint(transform.position).y > .95)
-        {
-            stopCamera = true;
-        }
-        else
-        {
-            stopCamera = false;
-        }
     }
 
     void FixedUpdate()
     {
+    }
+
+    void checkMovement()
+    {
+        float x = -1;
+        float z = -1;
+
+        GameObject otherPlayerX = null;
+        GameObject otherPlayerZ = null;
+
+        //Loop through players and set x to greatest x distance, and y to greatest y distance between current player and any other player
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (Mathf.Abs(transform.position.x - players[i].transform.position.x) > x) // if the distance is greater than current
+            {
+                x = Mathf.Abs(transform.position.x - players[i].transform.position.x); // update x to the new greatest distance
+                otherPlayerX = players[i]; // set this gamobject to the other player that this player has the greatest distance with
+            }
+
+            if (Mathf.Abs(transform.position.z - players[i].transform.position.z) > z)
+            {
+                z = Mathf.Abs(transform.position.z - players[i].transform.position.z);
+                otherPlayerZ = players[i];
+            }
+        }
+
+        Debug.Log(gameObject.name + " X -> " + otherPlayerX.gameObject.name);
+        Debug.Log(gameObject.name + " Z -> " + otherPlayerZ.gameObject.name);
+        Debug.Log(" ");
+
+        if (x >= maxMovementX) // if the greatest distance if greater than what is allowed, stop movement
+        {
+            stopMovementX = true;
+
+            float playerXInput = Input.GetAxis(horizontalButton) * speed; // sets this variable to the current input the player is giving for horizontal movement
+
+            if (Mathf.Abs((transform.position.x + playerXInput) - otherPlayerX.transform.position.x) < maxMovementX) // if the input the player is giving plus his current x position is less than max movement
+            {
+                stopMovementX = false; // the player is allowed to move again because they will be less than the max movement allowed
+            }
+        }
+        else // if the greatest distance is still lower than what is allowed the player can move
+        {
+            stopMovementX = false;
+        }
+
+        if (z >= maxMovementZ)
+        {
+            stopMovementZ = true;
+
+            float playerZInput = Input.GetAxis(verticalButton) * speed; //Players input for Z axis
+
+            if (Mathf.Abs((transform.position.z + playerZInput) - otherPlayerZ.transform.position.z) < maxMovementZ)
+            {
+                stopMovementZ = false;
+            }
+        }
+        else
+        {
+            stopMovementZ = false;
+        }
     }
 }
